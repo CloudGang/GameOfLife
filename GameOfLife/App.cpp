72bb15a87f2124cd6731a9f7b1457a5dc1917999
Erik Faison <EFaison@Student.FullSail.edu>
2024-09-04 08:36:20 -0400

    1.6✔️

--- GameOfLife/App.cpp
+++ GameOfLife/App.cpp
@@ -1,6 +1,12 @@
 #include "App.h"
 #include "MainWindow.h"
 
+#define _CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+
+#define MEMORY_LEAK_LINE -1
+
 wxIMPLEMENT_APP(App);
 
 App::App() {
@@ -12,10 +18,18 @@ App::~App() {
 }
 
 bool App::OnInit() {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetBreakAlloc(MEMORY_LEAK_LINE);
 
 	//Instantiate the MainWindow pointer
 	mainWin = new MainWindow();
 	//Call the Show() method on the pointer 
 	mainWin->Show();
 	return true;
+}
+
+int App::OnExit()
+{
+	_CrtDumpMemoryLeaks();
+	return wxApp::OnExit();
 }
--- GameOfLife/App.h
+++ GameOfLife/App.h
@@ -12,5 +12,6 @@ public:
 	App();
 	~App();
 	virtual bool OnInit();
+	virtual int OnExit();
 };
 
